ff9d3b70be9d70d76ba028e05c207db265a4844d
HackHero <128874952+Sportsings@users.noreply.github.com>
2023-04-21 21:20:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -78,8 +78,9 @@ def parse(tokens):
                      node = expression()
                      token = tokens.pop(0)
                      if token[1] != ')':
-                         raise SyntaxError("Se esperaba ';'")
-                         return node
+                         if token[1] != ';':
+                             raise SyntaxError("Se esperaba ';'")
+                     return node
                 else:
                      raise SyntaxError("Se esperaba un número, un identificador, un string o '('")
                     # Procesa una lista de factores separados por '*' o '/'
